00fc6621358df8d22d7fcb26b55bffdea5f03c72
Ty TN <tytn9197@gmail.com>
2025-03-22 19:12:26 +0700

    add icon logo

--- src/constants/ICONS.tsx
+++ src/constants/ICONS.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ICWatchList from "#icons/ic_watchlist.svg";
 import ICHome from "#icons/ic_home.svg";
+import ICLogo from "#icons/ic_logo.svg";
+import { StyleProp, ViewStyle } from 'react-native';
 
 export type SvgProps = {
     color?: string;
     width?: number;
     height?: number;
+    style?: StyleProp<ViewStyle>;
 };
 
 export const ICONS = {
     IC_HOME: (props: SvgProps) => <ICHome {...props}/>,
     IC_WATCHLIST: (props: SvgProps) => <ICWatchList {...props}/>,
+    IC_LOGO: (props: SvgProps) => <ICLogo {...props}/>,
 }
